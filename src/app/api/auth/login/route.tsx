6231bbfde8ebe7d924e30f9695deff245f85536a
kyeongbobak <kyeong0914@gmail.com>
2025-02-21 09:38:05 +0900

    chore : 주석처리 제거

--- src/app/api/auth/login/route.tsx
+++ src/app/api/auth/login/route.tsx
@@ -1,56 +1,56 @@
-// import { PrismaClient } from "@prisma/client";
-// import { NextRequest, NextResponse } from "next/server";
-// import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-// const prisma = new PrismaClient();
-// const JWT_SECRET = process.env.JWT_SECRET!;
+const prisma = new PrismaClient();
+const JWT_SECRET = process.env.JWT_SECRET!;
 
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { email, password } = await req.json();
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
 
-//     const user = await prisma.user.findUnique({
-//       where: { email },
-//     });
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-//     if (!user) {
-//       return NextResponse.json({ error: "User not found" }, { status: 404 });
-//     }
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
 
-//     if (!user.password) {
-//       const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
-//         expiresIn: "1h",
-//       });
+    if (!user.password) {
+      const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
+        expiresIn: "1h",
+      });
 
-//       return NextResponse.json({ message: "Login successful (email only)", token }, { status: 200 });
-//     }
+      return NextResponse.json({ message: "Login successful (email only)", token }, { status: 200 });
+    }
 
-//     if (password) {
-//       const isMatch = await bcrypt.compare(password, user.password);
+    if (password) {
+      const isMatch = await bcrypt.compare(password, user.password);
 
-//       if (!isMatch) {
-//         return NextResponse.json(
-//           { error: "Invalid password" },
-//           {
-//             status: 401,
-//           }
-//         );
-//       }
-//     }
+      if (!isMatch) {
+        return NextResponse.json(
+          { error: "Invalid password" },
+          {
+            status: 401,
+          }
+        );
+      }
+    }
 
-//     const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
-//       expiresIn: "1h",
-//     });
+    const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
 
-//     return NextResponse.json(
-//       { message: "Login successful", token },
-//       {
-//         status: 200,
-//       }
-//     );
-//   } catch (error) {
-//     console.log("Error fetching data:", error);
-//     return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
-//   }
-// }
+    return NextResponse.json(
+      { message: "Login successful", token },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
+  }
+}
--- src/app/api/postdetail/[id]/route.tsx
+++ src/app/api/postdetail/[id]/route.tsx
@@ -1,18 +1,18 @@
-// import { PrismaClient } from "@prisma/client";
-// import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
-//   try {
-//     const { id } = await params;
-//     const postId = Number(id);
-//     const selectedPost = await prisma.post.findUnique({
-//       where: { post_id: postId },
-//     });
-//     return NextResponse.json(selectedPost, { status: 200 });
-//   } catch (error) {
-//     console.log("Error fetching data:", error);
-//     return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
-//   }
-// }
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  try {
+    const { id } = await params;
+    const postId = Number(id);
+    const selectedPost = await prisma.post.findUnique({
+      where: { post_id: postId },
+    });
+    return NextResponse.json(selectedPost, { status: 200 });
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
+  }
+}
